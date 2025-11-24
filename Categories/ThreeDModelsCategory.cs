0784fd85037e8eed3ef0aad8063435b48626c73f
Andrey Rybakov <24fisher@gmail.com>
2025-11-24 19:10:09 +0300

    Enhance file type handling and reporting

--- Categories/ThreeDModelsCategory.cs
+++ Categories/ThreeDModelsCategory.cs
@@ -4,7 +4,7 @@ public sealed class ThreeDModelsCategory : FileCategory
 {
     private static readonly string[] ModelExtensions =
     [
-        ".obj", ".fbx", ".stl", ".3ds", ".dae", ".ply"
+        ".obj", ".fbx", ".stl", ".3ds", ".dae", ".ply", ".blend", ".gltf", ".glb", ".usdz", ".step", ".stp", ".iges", ".igs"
     ];
 
     public ThreeDModelsCategory()
