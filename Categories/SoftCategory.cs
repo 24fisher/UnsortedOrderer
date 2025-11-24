773a5a54e7d98467cba8ee478d20321e1c2caecc
Andrey Rybakov <24fisher@gmail.com>
2025-11-25 00:02:13 +0300

    Handle disk images in Soft subfolder

--- Categories/SoftCategory.cs
+++ Categories/SoftCategory.cs
@@ -4,11 +4,37 @@ namespace UnsortedOrderer.Categories;
 
 public sealed class SoftCategory : FileCategory, INonSplittableDirectoryCategory
 {
+    private const string DiskImagesFolderName = "_Образы дисков";
+
     private static readonly SoftwareDistributivesDetector DistributionDetector = new();
-    private static readonly string[] SoftExtensions =
+    private static readonly string[] DiskImageExtensions =
     [
-        ".exe", ".msi", ".msix", ".apk", ".dmg", ".pkg", ".deb", ".rpm", ".appimage", ".iso"
+        ".iso",
+        ".cso",
+        ".dmg",
+        ".img",
+        ".mdf",
+        ".mds",
+        ".nrg",
+        ".ccd",
+        ".isz",
+        ".vhd",
+        ".vhdx"
     ];
+
+    private static readonly string[] SoftExtensions =
+        [
+            ".exe",
+            ".msi",
+            ".msix",
+            ".apk",
+            ".pkg",
+            ".deb",
+            ".rpm",
+            ".appimage"
+        ]
+        .Concat(DiskImageExtensions)
+        .ToArray();
 
     public SoftCategory(string folderName)
         : base("Soft", folderName, SoftExtensions)
@@ -32,6 +58,15 @@ public sealed class SoftCategory : FileCategory, INonSplittableDirectoryCategory
     public string GetFileDestination(string destinationRoot, string filePath)
     {
         var categoryRoot = Path.Combine(destinationRoot, FolderName);
+        var extension = Path.GetExtension(filePath).ToLowerInvariant();
+        if (DiskImageExtensions.Contains(extension))
+        {
+            var diskImagesFolder = Path.Combine(categoryRoot, DiskImagesFolderName);
+            Directory.CreateDirectory(diskImagesFolder);
+
+            return diskImagesFolder;
+        }
+
         return DistributionFolderHelper.GetDistributionDestinationDirectory(categoryRoot, filePath);
     }
 }
